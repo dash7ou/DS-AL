3dcc2b3afbc4379fcd2305db302d2e4d6e45ae25
dash7ou <fxfddfno@gmail.com>
2023-05-01 13:22:18 +0300

    selection sort

--- javascript/sorting.js
+++ javascript/sorting.js
@@ -1,5 +1,6 @@
 console.log("---------------Question1 (Bubble Sort)-------------------");
 // T = O(n^2), S = O(1)
+// just compare and swap
 function bubbleSort(arr){
     for(let i = 0; i < arr.length; i++){
         for(let j = i+1; j < arr.length; j++){
@@ -21,6 +22,7 @@ console.log(bubbleSort([1]))
 
 console.log("---------------Question1 (Insertion Sort)-------------------");
 // T = O(n^2), S = O(1)
+// add elements to sorted array, more stable, less swapping, twice faster than bubble sort
 function insertionSort(arr){
     for(let i = 1; i < arr.length; i++){
         let j = i - 1;
@@ -39,3 +41,27 @@ console.log(insertionSort([7,2,3,8,1]))
 console.log(insertionSort([2,3,1,5]))
 console.log(insertionSort([1,2,3,4]))
 console.log(insertionSort([1]))
+
+
+console.log("---------------Question1 (Selection Sort)-------------------");
+// T = O(n^2), S = O(1)
+// find the smallest value and add it at the beginning.
+function selectionSort(arr){
+    for(let i = 0; i < arr.length; i++){
+        let smallest = i;
+        for(let j = i + 1; j < arr.length; j++){
+            if(arr[j] <= arr[smallest]){
+                smallest = j
+            }
+        }
+
+        [arr[smallest], arr[i]] = [arr[i], arr[smallest]];
+    }
+
+    return arr
+}
+
+console.log(selectionSort([7,2,3,8,1]))
+console.log(selectionSort([2,3,1,5]))
+console.log(selectionSort([1,2,3,1,4]))
+console.log(selectionSort([1]))
