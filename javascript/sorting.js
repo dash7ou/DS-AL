8bb9825b5102d4ec23e979646d31fd9e90636569
dash7ou <fxfddfno@gmail.com>
2023-05-03 15:57:57 +0300

    radix & counting sort

--- javascript/sorting.js
+++ javascript/sorting.js
@@ -1,4 +1,4 @@
-console.log("---------------Question1 (Bubble Sort)-------------------");
+console.log("---------------Question20 (Bubble Sort)-------------------");
 // T = O(n^2), S = O(1)
 // just compare and swap
 function bubbleSort(arr){
@@ -20,7 +20,7 @@ console.log(bubbleSort([2,3,1,5]))
 console.log(bubbleSort([1,2,3,4]))
 console.log(bubbleSort([1]))
 
-console.log("---------------Question1 (Insertion Sort)-------------------");
+console.log("---------------Question21 (Insertion Sort)-------------------");
 // T = O(n^2), S = O(1)
 // add elements to sorted array, more stable, less swapping, twice faster than bubble sort
 function insertionSort(arr){
@@ -43,7 +43,7 @@ console.log(insertionSort([1,2,3,4]))
 console.log(insertionSort([1]))
 
 
-console.log("---------------Question1 (Selection Sort)-------------------");
+console.log("---------------Question22 (Selection Sort)-------------------");
 // T = O(n^2), S = O(1)
 // find the smallest value and add it at the beginning.
 function selectionSort(arr){
@@ -66,7 +66,7 @@ console.log(selectionSort([2,3,1,5]))
 console.log(selectionSort([1,2,3,1,4]))
 console.log(selectionSort([1]))
 
-console.log("---------------Question1 (Merge Sort)-------------------");
+console.log("---------------Question23 (Merge Sort)-------------------");
 // Merge Sort T = O(nlogn), S = O(n)
 // Merge T & S = O(n+m)
 // divide array and merge sorted arrays, stable sorting algo
@@ -115,7 +115,7 @@ console.log(mergeSort([1,2,3,1,4]))
 console.log(mergeSort([1]))
 
 
-console.log("---------------Question1 (Quick Sort)-------------------");
+console.log("---------------Question24 (Quick Sort)-------------------");
 // Quick Sort - Recursively QS Lower Size, middle - pivot
 // T = O(nlogn)
 function swap(arr, i, j) {
@@ -179,3 +179,50 @@ console.log(quickSort([7,2,3,8,1]))
 console.log(quickSort([2,3,1,5]))
 console.log(quickSort([1,2,3,1,4]))
 console.log(quickSort([1]))
+
+console.log("---------------Question25 (Radix Sort)-------------------");
+
+function radixSort(arr){
+    if(arr.length === 0) return []
+    const greatestNumber = Math.max(...arr)
+    const numberOfDigits = Math.floor(Math.log10(greatestNumber));
+
+    for(let i =0; i <= numberOfDigits; i++){
+        countingSort(arr, i)
+    }
+
+    return arr;
+}
+
+
+function countingSort(arr, place){
+    const output = new Array(arr.length).fill(0)
+    const temp = new Array(10).fill(0)
+    const digitPlace = Math.pow(10, place)
+    
+    for(let i = 0; i < arr.length; i++){
+        const digit = Math.floor(arr[i] / digitPlace) % 10
+        temp[digit]++;
+    }
+
+
+    for(let i = 0; i < temp.length; i++){
+        temp[i] = temp[i-1] ? temp[i] + temp[i-1] : temp[i]
+    }
+
+    for(let j = arr.length-1; j>=0; j--){
+        const digit = Math.floor(arr[j] / digitPlace) % 10
+        temp[digit]--;
+        output[temp[digit]] = arr[j]
+    }
+    // console.log(output)
+    for(let i =0; i<arr.length; i++){
+        arr[i] = output[i]
+    }
+}
+
+console.log(radixSort([73,183,384,374,65,185,247]))
+console.log(radixSort([7,2,3,8,1]))
+console.log(radixSort([2,3,1,5]))
+console.log(radixSort([1,2,3,1,4]))
+console.log(radixSort([1]))
