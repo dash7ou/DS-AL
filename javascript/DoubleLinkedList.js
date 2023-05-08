6445d5b57f492b8450174867c1a664d02f45a465
dash7ou <fxfddfno@gmail.com>
2023-05-08 21:20:02 +0300

    insert by position

--- javascript/DoubleLinkedList.js
+++ javascript/DoubleLinkedList.js
@@ -17,6 +17,59 @@ class DoublyLinkedList{
         this.tail = null
     }
 
+    insert(node, position) {
+        if(!this.head){
+            this.head = node
+            this.tail = node
+            return
+        }
+
+        let counter = 0;
+        let current = this.head;
+        while(current){
+            if(counter == position){
+                if(current === this.head){
+                    node.next = current
+                    current.prev = node
+                    this.head = node
+                    return this
+                }
+
+                if(current === this.tail){
+                    node.prev = current
+                    current.next = node
+                    this.tail = node
+                    return this
+                }
+                
+                const prevCurrent = current.prev
+                const nextCurrent = current.next
+                current.prev.next=node
+                current.next = nextCurrent
+                current.prev = node
+                node.next = current
+                node.prev = prevCurrent
+
+                return this
+            }
+
+            if(current.next){
+                current = current.next
+                counter++;
+            }else{
+                break
+            }
+
+
+        }
+
+        node.prev = current
+        current.next = node
+        this.tail = node
+
+        return this
+    }
+
     // O(1)
     insertB(nodePosition,nodeInsert){
         if(this.head===nodeInsert && this.tail === nodeInsert){
@@ -98,10 +151,23 @@ linkedListDoubly.head = one;
 linkedListDoubly.tail = five;
 
 // console.log(linkedListDoubly.remove(one))
-console.log(linkedListDoubly.remove(five))
+// console.log(linkedListDoubly.remove(five))
 // console.log(linkedListDoubly.remove(three))
 
 // linkedListDoubly.insertB(two,three);
 
-linkedListDoubly.insertB(three,seven);
-linkedListDoubly.removeByValue(7)
+// linkedListDoubly.insertB(three,seven);
+// linkedListDoubly.removeByValue(7)
+
+
+const linkedListDoubly2 = new DoublyLinkedList();
+linkedListDoubly2.insert(new Node(3),10)
+linkedListDoubly2.insert(new Node(2),0)
+linkedListDoubly2.insert(new Node(1),0)
+linkedListDoubly2.insert(new Node(7),1)
+linkedListDoubly2.insert(new Node(8),4)
+console.log(linkedListDoubly2)
+linkedListDoubly2.insert(new Node(10),10)
+
+console.log(linkedListDoubly2)
+// console.log(linkedListDoubly2.head.next)
