c75ca7ca5ba475f4852aa23dd646c1c5c17be235
dash7ou <fxfddfno@gmail.com>
2023-05-10 15:50:54 +0300

    solve question on stacks

--- javascript/Stack.js
+++ javascript/Stack.js
@@ -20,13 +20,7 @@ class Stack {
             this.size++
             return
         }
-        // if(this.first == this.end){
-        //     console.log("here")
-        //     this.first = newNode
-        //     newNode.next = this.last
-        //     this.size++;
-        //     return
-        // }
+
         const oldFirst = this.first
         this.first = newNode
         newNode.next = oldFirst
@@ -49,9 +43,43 @@ const stack = new Stack()
 stack.addAtTheBegining(10)
 stack.addAtTheBegining(20)
 stack.addAtTheBegining(30)
-console.log(stack)
+// console.log(stack)
 
 console.log(stack.removeFromTheBeginning())
 console.log(stack.removeFromTheBeginning())
 console.log(stack.removeFromTheBeginning())
-console.log(stack)
+// console.log(stack)
+
+
+console.log("---------------Question35 ( Reverse Polish Notation )-------------------");
+function evalRPN(arr){
+    const stack = []
+    const validOperator = {
+        '+':(n1,n2)=>n1+n2,
+        '-':(n1,n2)=>n1-n2,
+        '*':(n1,n2)=>n1*n2,
+        '/':(n1,n2)=>Math.trunc(n1/n2)
+    };
+    
+    while(arr.length > 0){
+        let step = arr.shift()
+        if(!validOperator[step]){
+            stack.push(+step)
+            // step = arr.shift()
+        }else{
+            const number2 = stack.pop()
+            const number1 = stack.pop()
+    
+            const operatorResult = validOperator[step](number1, number2)
+            stack.push(parseInt(operatorResult))
+        }
+
+    }
+
+    return stack.pop()
+}
+
+
+console.log(evalRPN(["5","6","4", "/", "+", "1", "-"]))
+console.log(evalRPN(["4", "13", "5", "/", "+"]))
+console.log(evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
