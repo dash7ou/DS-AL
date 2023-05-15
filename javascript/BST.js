23992ace24220fe649e1a6c1e8a6570fcbf68eb1
dash7ou <fxfddfno@gmail.com>
2023-05-15 13:46:50 +0300

    get start with BST

---
+++ javascript/BST.js
@@ -0,0 +1,28 @@
+class Node {
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null
+    }
+
+
+}
+
+
+
+//                     20
+//                    /   \
+//                    6    35
+//                   / \   /  \ 
+//                   3  8  27  55
+//                  / \    / \   \
+//                  1  3   25 29  60
+
+let bst = new BinarySearchTree();
