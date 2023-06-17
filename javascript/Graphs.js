6f88cc7573dccc64ce08bdac809ae31ff0ef2b90
dash7ou <fxfddfno@gmail.com>
2023-06-17 19:28:11 +0300

    BFS adjacency Matrix

--- javascript/Graphs.js
+++ javascript/Graphs.js
@@ -1,5 +1,4 @@
 console.log("--------------- Question48 ( BFS - Adj List and Adj Matrix ) -------------------");
-// T = O(V+E), S = O(V)
 
 const adjacencyList ={
     'A':['B','F'],
@@ -10,6 +9,7 @@ const adjacencyList ={
     'F':['A','B','E']
 }
 
+// T = O(V+E), S = O(V)
 const travBFS = function(graph,start){
     const output = [];
     const queue = [start];
@@ -35,3 +35,50 @@ const travBFS = function(graph,start){
 
 
 console.log(travBFS(adjacencyList,'A'));
+
+console.log("----------------------------------------------------------------------------------")
+
+const vertices = ['A','B','C','D','E','F'];
+
+const vertexIndices ={
+    'A':0,
+    'B':1,
+    'C':2,
+    'D':3,
+    'E':4,
+    'F':5
+}
+
+const adjacencyMatrix = [
+    [0,1,0,0,0,1],
+    [1,0,1,0,0,1],
+    [0,1,0,1,1,0],
+    [0,0,1,0,1,0],
+    [0,0,1,1,0,1],
+    [1,1,0,0,1,0]
+]
+
+// T O(v^2), S O(V)
+const travMBFS = function(graph,start){
+    const visited = {};
+    const output = [];
+    const queue = [start];
+    visited[start] = true
+
+    while(queue.length){
+        const current = queue.shift();
+        output.push(current);
+        const neighbours = graph[vertexIndices[current]];
+        for(let i = 0; i < neighbours.length; i++){
+            const n = neighbours[i];
+            if(n === 1 && !visited[vertices[i]]){
+                queue.push(vertices[i])
+                visited[vertices[i]] = true
+            }
+        }
+    }
+
+    return output
+}
+
+console.log(travMBFS(adjacencyMatrix,'A'));
